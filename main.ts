e0a42cfa9520a43102c0c1541a565b441e4f8995
Altison <71751499+Altison@users.noreply.github.com>
2020-09-23 15:27:38 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+for (let index = 0; index < 10; index++) {
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.clearScreen()
+    basic.pause(200)
+}
