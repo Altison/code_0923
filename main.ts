ad777d0123112c122c73ff602b6879aa2db37aa5
Altison <71751499+Altison@users.noreply.github.com>
2020-09-23 15:35:30 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,1 @@
-for (let index = 0; index < 10; index++) {
-    basic.showLeds(`
-        . . # . .
-        # # # # #
-        . . # . .
-        . # . # .
-        . . . . .
-        `)
-    basic.pause(200)
-    basic.clearScreen()
-    basic.pause(200)
-}
+
